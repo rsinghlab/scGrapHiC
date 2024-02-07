6416a69f152c5d106e9fe4d41df0a834e77b833c
Murtaza <gmurtaza404@gmail.com>
2024-02-07 15:36:33 -0500

    rearanged the code

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
+import torch
 
 import numpy as np
 import lightning.pytorch as pl
 
-from src.download_datasets import *
-
-
 from src.globals import *
+from src.dataset_creator import *
+from src.download_datasets import *
+from src.preprocess_datasets import *
 from src.model import GenomicDataset, scGrapHiC
-from src.dataset_creator import create_schires_dataset, create_schic_pseudobulk_dataset, create_cell_type_dataset
-from src.download_datasets import create_directory_structure
-from src.preprocess_datasets import preprocess_hires_datasets
 from src.pseudobulk import create_pseudobulk_files
-from src.visualizations import *
-from lightning.pytorch.loggers import TensorBoardLogger
 from src.utils import initialize_parameters_from_args
+
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
+
+from src.visualizations import *
 from src.evaluations import evaluate
 
 
@@ -38,66 +39,81 @@ create_pseudobulk_files(HIRES_BRAIN_METADATA_FILE)
 create_pseudobulk_files(HIRES_EMBRYO_METADATA_FILE)
 
 
-# exclusion_set = ['EX15', 'brain']
-# create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'train', PARAMETERS['experiment'])
-# create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'valid', PARAMETERS['experiment'])
-# create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'test', PARAMETERS['experiment'])
+exclusion_set = ['EX15', 'brain']
+create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'train', PARAMETERS['experiment'])
+create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'valid', PARAMETERS['experiment'])
+create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'test', PARAMETERS['experiment'])
 
+exclusion_set = ['E70', 'E75', 'E80', 'E85', 'E95', 'EX05']
+create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'ood', PARAMETERS['experiment'])
 
+train_dataset = GenomicDataset(
+    os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_train.npz'.format(PARAMETERS['experiment'])),
+    PARAMETERS
+)
+valid_dataset = GenomicDataset(
+    os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_valid.npz'.format(PARAMETERS['experiment'])),
+    PARAMETERS
+)
+test_dataset = GenomicDataset(
+    os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_test.npz'.format(PARAMETERS['experiment'])),
+    PARAMETERS
+)
 
-# exclusion_set = ['E70', 'E75', 'E80', 'E85', 'E95', 'EX05']
-# create_schic_pseudobulk_dataset(exclusion_set, PARAMETERS, 'ood', PARAMETERS['experiment'])
+ood_dataset = GenomicDataset(
+    os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_ood.npz'.format(PARAMETERS['experiment'])),
+    PARAMETERS
+)
 
+train_data_loader =  torch.utils.data.DataLoader(train_dataset, PARAMETERS['batch_size'], shuffle=True)
+validation_data_loader =  torch.utils.data.DataLoader(valid_dataset, PARAMETERS['batch_size'], shuffle=False)
+test_data_loader = torch.utils.data.DataLoader(test_dataset, PARAMETERS['batch_size'], shuffle=False)
+ood_data_loader = torch.utils.data.DataLoader(ood_dataset, PARAMETERS['batch_size'], shuffle=False)
 
-# train_dataset = GenomicDataset(
-#     os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_train.npz'.format(PARAMETERS['experiment'])),
-#     PARAMETERS
-# )
-# valid_dataset = GenomicDataset(
-#     os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_valid.npz'.format(PARAMETERS['experiment'])),
-#     PARAMETERS
-# )
-# test_dataset = GenomicDataset(
-#     os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_test.npz'.format(PARAMETERS['experiment'])),
-#     PARAMETERS
-# )
+tb_logger = TensorBoardLogger("logs", name=PARAMETERS['experiment'])
+checkpoint_callback = ModelCheckpoint(monitor="valid/SCC",  save_top_k=3, mode='max')
+scgraphic = scGrapHiC(PARAMETERS)
 
-# ood_dataset = GenomicDataset(
-#     os.path.join(MOUSE_PROCESSED_DATA_HIRES, '{}_ood.npz'.format(PARAMETERS['experiment'])),
-#     PARAMETERS
-# )
+trainer = pl.Trainer(
+    max_epochs=PARAMETERS['epochs'], 
+    check_val_every_n_epoch=50, 
+    logger=tb_logger,
+    deterministic=True,
+    callbacks=[checkpoint_callback],
+    gradient_clip_val=PARAMETERS['gradient_clip_value'],
+)
 
-# visualize_hic_contact_matrix(train_dataset[15]['targets'], 'visualizations/target.png')
-
-
-# train_data_loader =  torch.utils.data.DataLoader(train_dataset, PARAMETERS['batch_size'], shuffle=True)
-# validation_data_loader =  torch.utils.data.DataLoader(valid_dataset, PARAMETERS['batch_size'], shuffle=False)
-# test_data_loader = torch.utils.data.DataLoader(test_dataset, PARAMETERS['batch_size'], shuffle=False)
-# ood_data_loader = torch.utils.data.DataLoader(ood_dataset, PARAMETERS['batch_size'], shuffle=False)
-
-# tb_logger = TensorBoardLogger("logs", name=PARAMETERS['experiment'])
-# checkpoint_callback = ModelCheckpoint(monitor="valid/SCC",  save_top_k=3, mode='max')
-# scgraphic = scGrapHiC(PARAMETERS)
-
-# trainer = pl.Trainer(
-#     max_epochs=PARAMETERS['epochs'], 
-#     check_val_every_n_epoch=50, 
-#     logger=tb_logger,
-#     deterministic=True,
-#     callbacks=[checkpoint_callback],
-#     gradient_clip_val=PARAMETERS['gradient_clip_value'],
-# )
-
-# trainer.fit(scgraphic, train_data_loader, validation_data_loader)
-
-# trainer.test(scgraphic, test_data_loader) 
-# trainer.test(scgraphic, ood_data_loader)
-
-# ckpt_path = 'logs/mesc-new/version_4/checkpoints/epoch=359-step=34560.ckpt'
+trainer.fit(scgraphic, train_data_loader, validation_data_loader)
+# Replace the .fit function with the lines below if you want to test on weights:
+# ckpt_path = 'path/to/scgraphic-model-weights'
 # checkpoint = torch.load(ckpt_path, map_location=lambda storage, loc: storage)
 # scgraphic.load_state_dict(checkpoint['state_dict'])
 
-# evaluate(os.path.join(RESULTS, PARAMETERS['experiment']), PARAMETERS)
+trainer.test(scgraphic, test_data_loader) 
+trainer.test(scgraphic, ood_data_loader)
+
+
+evaluate(os.path.join(RESULTS, PARAMETERS['experiment']), PARAMETERS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################################################################################################################################################
+
 
 # create_plots_for_figure2()
 # create_plots_for_figure3()
